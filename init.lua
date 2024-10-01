b7c93a98cdb30ce8d10e05d606fd428fa7e9e6b6
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-01 16:27:02 +0100

    Better compilation unit switching

--- init.lua
+++ init.lua
@@ -119,7 +119,7 @@ local function assign_files(long_files, cwd)
   return files
 end
 
-local function assign_file_types(files)
+local function assign_cc_file_types(files)
   local source = ""
   local header = ""
 
@@ -134,7 +134,7 @@ local function assign_file_types(files)
   return source, header
 end
 
-function switch_file_in_unit(dir)
+function switch_file_in_cc_unit(dir)
   if not string.match(vim.fn.expand("%:e"), "c") and not string.match(vim.fn.expand("%:e"), "h") then
     vim.notify("Not a source or header file!", "error")
     return
@@ -169,5 +169,87 @@ function switch_file_in_unit(dir)
     vim.notify("Unexpectedly high amount of corresponding files found!", "error")
   end
 end
-vim.keymap.set("n", "<LEADER>pu", function() switch_file_in_unit(vim.fn.getcwd() .. "/program") end)
+vim.keymap.set("n", "<LEADER>pU", function() switch_file_in_unit(vim.fn.getcwd() .. "/program") end)
                     -- Folder to recursively search for files in the compilation unit ^^^
+
+local function find_one_from_two(file1, file2)
+  if file1 ~= "" then
+    return file1
+  elseif file2 ~= "" then
+    return file2
+  end
+end
+
+local function assign_cxx_file_types(files)
+  local source = ""
+  local header = ""
+  local inline = ""
+
+  for _, file in ipairs(files) do
+    if string.match(file, "%.cpp$") then
+      source = file
+    elseif string.match(file, "%.hpp$") then
+      header = file
+    elseif string.match(file, "%.inl$") then
+      inline = file
+    end
+  end
+
+  return source, header, inline
+end
+
+function switch_file_in_cxx_unit(dir)
+  if not string.match(vim.fn.expand("%:e"), "cpp") and not string.match(vim.fn.expand("%:e"), "hpp") and not string.match(vim.fn.expand("%:e"), "inl") then
+    vim.notify("Not a source, header or inline file!", "error")
+    return
+  end
+
+  local cwd = vim.fn.getcwd()
+  local name = vim.fn.expand("%:t:r")
+  local long_files = vim.fn.globpath(dir, "**/" .. name .. ".*", 0, 1)
+  local files = assign_files(long_files, cwd)
+
+  if #files == 0 then
+    vim.notify("Problem reading filename!", "error")
+
+  elseif #files == 1 then
+    vim.notify("There is only one file in this compilation unit!", "error")
+
+  elseif #files == 2 then
+    local source, header, inline = assign_cxx_file_types(files)
+    local other_file = ""
+
+    if string.match(vim.fn.expand("%:e"), "cpp") then
+      other_file = find_one_from_two(header, inline)
+    elseif string.match(vim.fn.expand("%:e"), "hpp") then
+      other_file = find_one_from_two(source, inline)
+    elseif string.match(vim.fn.expand("%:e"), "inl") then
+      other_file = find_one_from_two(source, header)
+    else
+      vim.notify("Unexpected file extension!", "error")
+      return
+    end
+    vim.cmd("edit " .. other_file)
+
+  elseif #files == 3 then
+    local source, header, inline = assign_cxx_file_types(files)
+    local selection = {}
+
+    if string.match(vim.fn.expand("%:e"), "cpp") then
+      selection = {header, inline}
+    elseif string.match(vim.fn.expand("%:e"), "hpp") then
+      selection = {source, inline}
+    elseif string.match(vim.fn.expand("%:e"), "inl") then
+      selection = {source, header}
+    else
+      vim.notify("Unexpected file extension!", "error")
+      return
+    end
+    vim.ui.select(selection, {prompt = "Choose a file:"}, function(choice) if choice then vim.cmd("edit " .. choice) end end)
+
+  else
+    vim.notify("Unexpectedly high amount of corresponding files found!", "error")
+  end
+end
+vim.keymap.set("n", "<LEADER>pu", function() switch_file_in_cxx_unit(vim.fn.getcwd() .. "/program") end)
+                       -- Folder to recursively search for files in the compilation unit ^^^

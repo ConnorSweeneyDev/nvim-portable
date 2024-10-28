4987d9c80ef70255c6b47c70a77c7c5bb50775b0
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-28 11:48:23 +0000

    Abbreviations for keymap, opt, api and g

--- init.lua
+++ init.lua
@@ -1,96 +1,101 @@
+map = vim.keymap.set
+opt = vim.opt
+api = vim.api
+g = vim.g
+
 -- REMAP
-vim.g.mapleader = " "
-vim.keymap.set("n", "<C-f>", "<nop>")
-vim.keymap.set("i", "<C-c>", "<Esc>")
+g.mapleader = " "
+map("n", "<C-f>", "<nop>")
+map("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<LEADER>w", "<CMD>w<CR>")
-vim.keymap.set("n", "<LEADER><LEADER>", "<CMD>so<CR>")
-vim.keymap.set("n", "<LEADER>pv", "<CMD>Ex<CR>")
-vim.keymap.set("n", "<LEADER>tw", "<CMD>set wrap!<CR>")
+map("n", "<LEADER>w", "<CMD>w<CR>")
+map("n", "<LEADER><LEADER>", "<CMD>so<CR>")
+map("n", "<LEADER>pv", "<CMD>Ex<CR>")
+map("n", "<LEADER>tw", "<CMD>set wrap!<CR>")
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "gJ", "mzgJ`z")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set({"n", "v"}, "j", "gj")
-vim.keymap.set({"n", "v"}, "k", "gk")
+map("n", "J", "mzJ`z")
+map("n", "gJ", "mzgJ`z")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map({"n", "v"}, "j", "gj")
+map({"n", "v"}, "k", "gk")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "*", "*zzzv")
-vim.keymap.set("n", "#", "#zzzv")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "*", "*zzzv")
+map("n", "#", "#zzzv")
 
-vim.keymap.set("n", "<A-h>", "<C-w>h")
-vim.keymap.set("n", "<A-j>", "<C-w>j")
-vim.keymap.set("n", "<A-k>", "<C-w>k")
-vim.keymap.set("n", "<A-l>", "<C-w>l")
+map("n", "<A-h>", "<C-w>h")
+map("n", "<A-j>", "<C-w>j")
+map("n", "<A-k>", "<C-w>k")
+map("n", "<A-l>", "<C-w>l")
 
-vim.keymap.set({"n", "v"}, "<LEADER>y", [["+y]])
-vim.keymap.set("n", "<LEADER>Y", [["+Y]])
-vim.keymap.set("x", "<LEADER>p", [["_dP]])
-vim.keymap.set({"n", "v"}, "<LEADER>d", [["_d]])
-vim.keymap.set({"n", "v"}, "<LEADER>c", [["_c]])
-vim.keymap.set({"n", "v"}, "<LEADER>x", [["_x]])
+map({"n", "v"}, "<LEADER>y", [["+y]])
+map("n", "<LEADER>Y", [["+Y]])
+map("x", "<LEADER>p", [["_dP]])
+map({"n", "v"}, "<LEADER>d", [["_d]])
+map({"n", "v"}, "<LEADER>c", [["_c]])
+map({"n", "v"}, "<LEADER>x", [["_x]])
 
-vim.keymap.set("n", "<LEADER>s", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>")
-vim.keymap.set("n", "<C-s>", ":%s//g<Left><Left>")
-vim.keymap.set("v", "<LEADER>s", "\"hy:%s/<C-r>h/<C-r>h/g<Left><Left>")
-vim.keymap.set("v", "<C-s>", ":s//g<Left><Left>")
-vim.keymap.set("v", "<C-n>", ":normal ")
+map("n", "<LEADER>s", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>")
+map("n", "<C-s>", ":%s//g<Left><Left>")
+map("v", "<LEADER>s", "\"hy:%s/<C-r>h/<C-r>h/g<Left><Left>")
+map("v", "<C-s>", ":s//g<Left><Left>")
+map("v", "<C-n>", ":normal ")
 
 -- Will specifically search in files in the program directory
-vim.keymap.set("n", "<LEADER>qg", ":silent grep  program/*/*<C-Left><Left>")
-vim.keymap.set("n", "<LEADER>qo", "<CMD>copen<CR>")
-vim.keymap.set("n", "<LEADER>qr", ":cdo s//g<Left><Left>")
-vim.keymap.set("n", "<LEADER>qw", "\"+yiw:silent grep <C-r><C-w> program/*/*<CR><CMD>copen<CR>")
-vim.keymap.set("n", "<LEADER>qW", "\"+yiw:silent grep <C-r><C-a> program/*/*<CR><CMD>copen<CR>")
-vim.keymap.set("v", "<LEADER>q", "\"+ygv\"hy:silent grep <C-r>h program/*/*<CR><CMD>copen<CR>")
+map("n", "<LEADER>qg", ":silent grep  program/*/*<C-Left><Left>")
+map("n", "<LEADER>qo", "<CMD>copen<CR>")
+map("n", "<LEADER>qr", ":cdo s//g<Left><Left>")
+map("n", "<LEADER>qw", "\"+yiw:silent grep <C-r><C-w> program/*/*<CR><CMD>copen<CR>")
+map("n", "<LEADER>qW", "\"+yiw:silent grep <C-r><C-a> program/*/*<CR><CMD>copen<CR>")
+map("v", "<LEADER>q", "\"+ygv\"hy:silent grep <C-r>h program/*/*<CR><CMD>copen<CR>")
 
-vim.keymap.set("n", "<LEADER>v", "<CMD>!./script/clean.sh<CR>")
-vim.keymap.set("n", "<LEADER>b", "<CMD>!./script/build.sh<CR>")
-vim.keymap.set("n", "<LEADER>n", "<CMD>!./script/run.sh<CR>")
-vim.keymap.set("n", "<LEADER>m", "<CMD>!./script/debug.sh<CR>")
+map("n", "<LEADER>v", "<CMD>!./script/clean.sh<CR>")
+map("n", "<LEADER>b", "<CMD>!./script/build.sh<CR>")
+map("n", "<LEADER>n", "<CMD>!./script/run.sh<CR>")
+map("n", "<LEADER>m", "<CMD>!./script/debug.sh<CR>")
 -- REMAP
 
 -- SET
-vim.g.netrw_banner = 0
+g.netrw_banner = 0
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "no"
-vim.opt.colorcolumn = "100"
+opt.nu = true
+opt.relativenumber = true
+opt.signcolumn = "no"
+opt.colorcolumn = "100"
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
 
-vim.opt.wrap = false
-vim.opt.linebreak = true
-vim.opt.scrolloff = 8
-vim.opt.textwidth = 100
-vim.opt.formatoptions:remove("t")
-vim.opt.formatoptions:remove("c")
+opt.wrap = false
+opt.linebreak = true
+opt.scrolloff = 8
+opt.textwidth = 100
+opt.formatoptions:remove("t")
+opt.formatoptions:remove("c")
 
-vim.opt.autoread = true
-vim.opt.swapfile = false
-vim.opt.undofile = true
-vim.opt.backup = false
-vim.opt.writebackup = false
+opt.autoread = true
+opt.swapfile = false
+opt.undofile = true
+opt.backup = false
+opt.writebackup = false
 
-vim.opt.ignorecase = false
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.ignorecase = false
+opt.hlsearch = false
+opt.incsearch = true
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
-vim.opt.isfname:append("@-@")
-vim.opt.updatetime = 300
+opt.isfname:append("@-@")
+opt.updatetime = 300
 
-vim.api.nvim_create_autocmd("VimResized", { command = "wincmd =" })
+api.nvim_create_autocmd("VimResized", { command = "wincmd =" })
 -- SET
 
 -- C/C++
@@ -155,7 +160,7 @@ function switch_file_in_cc_unit(dir)
     vim.notify("Unexpectedly high amount of corresponding files found!", "error")
   end
 end
-vim.keymap.set("n", "<LEADER>pU", function() switch_file_in_cc_unit(vim.fn.getcwd() .. "/program") end)
+map("n", "<LEADER>pU", function() switch_file_in_cc_unit(vim.fn.getcwd() .. "/program") end)
                       -- Folder to recursively search for files in the compilation unit ^^^
 
 local function find_one_from_two(file1, file2)
@@ -237,6 +242,6 @@ function switch_file_in_cxx_unit(dir)
     vim.notify("Unexpectedly high amount of corresponding files found!", "error")
   end
 end
-vim.keymap.set("n", "<LEADER>pu", function() switch_file_in_cxx_unit(vim.fn.getcwd() .. "/program") end)
+map("n", "<LEADER>pu", function() switch_file_in_cxx_unit(vim.fn.getcwd() .. "/program") end)
                        -- Folder to recursively search for files in the compilation unit ^^^
 -- C/C++
